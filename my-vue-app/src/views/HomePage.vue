9f31b9d48a4b2c830eef2d57af08f2d521719b77
UmutSemihSoyer <umutsemihsoyer@hotmail.com>
2023-10-26 15:07:19 +0300

    fix

--- my-vue-app/src/views/HomePage.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import appHeader from '/src/components/appHeader.vue'
-import appSidebar from '../components/appSidebar.vue';
-</script>
-
-<template>
-  <appHeader></appHeader>
-  <appSidebar></appSidebar>
-</template>
